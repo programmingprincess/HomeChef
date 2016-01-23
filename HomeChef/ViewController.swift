9025168370dab8c5f5a1fd6afe4696074f44af3a
Decontra <jch.duran1@gmail.com>
2016-01-23 12:26:52 -0700

    Julio's Commit

--- HomeChef/ViewController.swift
+++ HomeChef/ViewController.swift
@@ -7,22 +7,45 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var beginButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        beginButton.layer.cornerRadius = 5
+        
+        // Parameters
+        let ingredients = "bread,eggs,milk"
+        let limitLicense = false
+        let number = 2
+        let ranking = 1
+        
+        let url = NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=\(ingredients)&limitLicense=\(limitLicense)&number=\(number)&ranking=\(ranking)")
+        let headers = [
+            "X-Mashape-Key": "NHDnizUDH5mshbK6fNDHtFJAQRpDp1zYm3FjsnAVFgzGtVfsXi",
+            "Accept": "application/json"
+        ]
+        
+        // Request Handling
+        Alamofire.request(.GET, url!, headers: headers, encoding: .JSON).responseJSON {
+            // Response Handling
+            response in switch response.result {
+                case .Success:
+                    let json = JSON(response.result.value!)
+                    print("JSON: \(json)")
+                    print("Selected JSON: \(json[0])")
+                case .Failure(let error):
+                    print("Request failed with error: \(error)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    //testing the branch commit number 2 
 }
 
